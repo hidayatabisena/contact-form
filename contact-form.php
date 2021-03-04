acae833441b0e4c693d0fab9ae93f2a078d0780b
Adi Hernawan <adiherna2210@bsi.ac.id>
2021-03-04 13:28:38 +0700

    Add validation message in js and styling link back to form

--- contact-form.php
+++ contact-form.php
@@ -92,6 +92,7 @@ label,a
 	frmvalidator.addValidation("name","req","Please provide your name"); 
 	frmvalidator.addValidation("email","req","Please provide your email"); 
 	frmvalidator.addValidation("email","email","Please enter a valid email address"); 
+	frmvalidator.addValidation("message","req","Please provide your message"); 
 
 </script>
 
